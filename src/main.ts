a96ab67d2b568e994af9dbb87e8f672e7b5efa01
Guilherme Maier <guilherme@enzo.com.br>
2024-12-18 08:59:54 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import dayjs from 'dayjs';
+import { json } from 'express';
+import * as helmet from 'helmet';
+import * as requestIp from 'request-ip';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(requestIp.mw());
   app.enableCors();
+  app.use(helmet());
+
+  app.use(json({ limit: '50mb' }));
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
   const config = new DocumentBuilder()
     .setTitle('Your API')
     .setDescription('API description')
-    .setVersion('1.0')
+    .setVersion(dayjs().format('HH:mm:ss DD-MM-YYYY'))
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
